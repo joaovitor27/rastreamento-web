628cbec4df144251ac7f89a0f89b1a64209053c5
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2023-01-30 22:32:39 -0300

    corrigindo interptor axios

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,13 +1,12 @@
-import axios, {AxiosError, AxiosRequestConfig} from 'axios';
+import axios, {AxiosError, InternalAxiosRequestConfig} from 'axios';
 
 const api = axios.create({
     baseURL: process.env.VUE_APP_URL_API,
 });
 
 api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
+    (config: InternalAxiosRequestConfig) => {
         config.headers.Authorization = process.env.VUE_APP_API_KEY;
-
         return config;
     },
     (error: AxiosError) => {
